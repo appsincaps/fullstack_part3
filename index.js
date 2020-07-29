a70258ade8dbdf5956f31e4c09bb52126663580d
Sergey Frolov <sergey.frolov@sunlightphotonics.com>
2020-07-29 18:19:03 -0400

    phonebook part3 step4 is done

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ app.get('/info', (req, res) => {
             </div>`)
 })
 
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
+
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
@@ -29,8 +33,10 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
-app.get('/api/persons', (req, res) => {
-  res.json(persons)
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  res.status(204).end()
 })
 
 const PORT = 3001
